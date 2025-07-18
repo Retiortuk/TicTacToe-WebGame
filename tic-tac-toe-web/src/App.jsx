d955d82c23b1f18683bed76f81e6b72cc0515e3c
Retiortuk <aauliagazzam@gmail.com>
2025-07-18 14:29:08 +0700

    feat: x joined the club

--- tic-tac-toe-web/src/App.jsx
+++ tic-tac-toe-web/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 
 
 function Square() {
+  const [value, setValue] = useState(null);
+
+  // Function for handle the click 
+  function handleClick() {
+    setValue('x'); // Wil set the default value as x when button is clicked 
+  }
+
   return (
-    <button className='square shadow'>X</button>
+    <button className='square shadow' onClick={handleClick}>{value}</button>
   )
 }
 
 export default function App() {
+  const [isXNext, setXisNext] = useState();
 
   return (
     <>
@@ -16,18 +24,18 @@ export default function App() {
           <div className="headlineGames text-center justify-content-center align-items-center">
             <h1>Tic-tac-toe Games</h1>
             <p className='fst-italic text-white text-opacity-75'>
-              <small>Created By: Retiortuk Using Javascript and React</small></p>
+              <small>Created By : Retiortuk Using Javascript and React</small></p>
           </div>
           <div className="board">
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
+            <Square value='' />
+            <Square value='' />
+            <Square value='' />
+            <Square value='' />
+            <Square value='' />
+            <Square value='' />
+            <Square value='' />
+            <Square value='' />
+            <Square value='' />
           </div>
           <h5 className='turnAndWin'>Who's Turn and who's Won:</h5>
           <div>
