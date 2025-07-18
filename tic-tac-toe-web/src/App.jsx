6f7569d358c2f683f68db207336ee633f7e33025
Retiortuk <aauliagazzam@gmail.com>
2025-07-18 10:33:19 +0700

    button

--- tic-tac-toe-web/src/App.jsx
+++ tic-tac-toe-web/src/App.jsx
@@ -1,13 +1,33 @@
 import { useState } from 'react'
-import './App.css'
 
-function App() {
+
+function Square() {
+  return (
+    <button className='square shadow'>X</button>
+  )
+}
+
+export default function App() {
 
   return (
     <>
-      
+      <div className="container">
+        <div className="outer-container justify-content-center align-items-center">
+          <h1 className='headlineGames text-center'>Tic-tac-toe Games</h1>
+          <div className="board">
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
 
-export default App
